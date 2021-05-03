7012363200044453c4d6e2f325af2904fa8badcf
Rafael Aquino <rafaaquino28@gmail.com>
2021-05-03 20:37:16 -0300

    Spec Test Calculator Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { from } from 'rxjs';
-
 import { AppComponent } from './app.component';
 import { CalculadoraModule } from './calculadora/calculadora.module'
 
