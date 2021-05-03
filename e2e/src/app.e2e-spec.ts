4b3efd80e091d47815cb656fd1c2bc325e106440
Rafael Aquino <rafaaquino28@gmail.com>
2021-05-03 16:41:58 -0300

    Spec Test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -13,11 +13,11 @@ describe('workspace-project App', () => {
   //  expect(await page.getTitleText()).toEqual('calculadora app is running!');
   //});
 
-  afterEach(async () => {
+  //afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  //  const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+  //  expect(logs).not.toContain(jasmine.objectContaining({
+  //    level: logging.Level.SEVERE,
+  //  } as logging.Entry));
+ // });
 });
